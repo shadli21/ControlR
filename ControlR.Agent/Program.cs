81c99564bdf7e7a1f98cf6789c71752ef72639e8
Jared Goodwin <translucency@outlook.com>
2026-02-11 18:04:30 -0800

    Replace wasm-progress helper with built-in framework classes. (#76)

--- ControlR.Agent/Program.cs
+++ ControlR.Agent/Program.cs
@@ -3,9 +3,9 @@ using System.CommandLine;
 
 var rootCommand = new RootCommand("Open-source remote control agent.")
 {
-    CommandProvider.GetInstallCommand(args),
-    CommandProvider.GetRunCommand(args),
-    CommandProvider.GetUninstallCommand(args),
+		CommandProvider.GetInstallCommand(args),
+		CommandProvider.GetRunCommand(args),
+		CommandProvider.GetUninstallCommand(args),
 };
 
 var parseResult = rootCommand.Parse(args);
