246a2378febc83d3ede7b6890cb05c2fc55af324
Jared Goodwin <translucency@outlook.com>
2026-07-23 16:25:17 -0700

    Promote `LogonToken` to EF entity.

---
+++ ControlR.Web.Server/Data/Entities/LogonToken.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using ControlR.Web.Server.Data.Entities.Bases;
+
+namespace ControlR.Web.Server.Data.Entities;
+
+public class LogonToken : TenantEntityBase
+{
+  public Device? Device { get; set; }
+  public Guid DeviceId { get; set; }
+
+  public DateTimeOffset ExpiresAt { get; set; }
+
+  public bool IsConsumed { get; set; }
+
+  [StringLength(32)]
+  public string? Prefix { get; set; }
+
+  [StringLength(100)]
+  public string? SessionCorrelationId { get; set; }
+
+  [StringLength(256)]
+  public required string Token { get; set; }
+
+  public AppUser? User { get; set; }
+
+  [StringLength(100)]
+  public string? UserCorrelationId { get; set; }
+
+  public Guid UserId { get; set; }
+}
